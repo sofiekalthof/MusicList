7886cc282e3bda9c0ef0868fc58e3a14307b70e0
sofiekalthof <sofie.kalthof@stud.uni-due.de>
2023-06-08 14:27:15 +0200

    Task 1.3

--- backend/src/dbPopulate.js
+++ backend/src/dbPopulate.js
@@ -1,16 +1,13 @@
 const musicListModel = require("./dbMusicSchema.js");
 
 // Task 1.3 Starts here
-const exampleMussicList = [
-    {"category": "rock", "title": "It's My Life", "url": "https://www.youtube.com/watch?v=vx2u5uUu3DE"},
-    {"category": "rock", "title": "Seven Nation Army", "url": "https://www.youtube.com/watch?v=0J2QdDbelmY"},
-    {"category": "rock", "title": "Viva La Vida", "url": "https://www.youtube.com/watch?v=dvgZkm1xWPE"}
-];
+
+// Task 1.3 Continues
 
 async function populateDatabase(){
     try {
         // Task 1.3 Continues
-        await musicListModel.insertMany(exampleMussicList);
+
         // Task 1.3 Ends here
         
         console.log("Database populated");
